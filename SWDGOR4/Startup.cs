0a0150fc7a51efbfc9229b369d1e66e1f6b1b9e0
balti003 <balti003@edu.xamk.fi>
2019-09-25 13:49:09 +0300

    XAMK. fi open this link to open it

--- SWDGOR4/Startup.cs
+++ SWDGOR4/Startup.cs
@@ -29,7 +29,7 @@ namespace SWDGOR4
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello XAMK!");
             });
         }
     }
